4e809f3d0328bb414801e4fdc88fc28933b4a1d2
Brooke Elliott <brookeelliott711@gmail.com>
2023-07-07 18:32:30 -0600

    static exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double cTemp = TempConverter.FahrenheitToCelcius(23.23);
+            double fTemp = TempConverter.CelciusToFahrenheit(42.42);
+
+            Console.WriteLine($"{cTemp} degrees C ---- {fTemp} Degrees F");
+            Console.WriteLine("We just converted some temp values!! Yay!! Not lets all agree Celcius is better and the US is weird for using F.");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+		//public TempConverter()
+		//{
+		//}
+
+		public static double FahrenheitToCelcius(double temp)
+		{
+			return ((temp - 32) * 1.8);
+		}
+
+        public static double CelciusToFahrenheit(double temp)
+        {
+			return ((temp * 1.8) + 32);
+        }
+    }
+}
+
